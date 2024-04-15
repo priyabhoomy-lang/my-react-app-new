bbd33b4d51d9a2b79daa7cb0a45af7b63e73e944
priyabhoomy-lang <saran.pk@gmail.com>
2024-04-15 12:54:17 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Matt Delac
+            Priya Bhoomy
           </a>{" "}
           and is{" "}
           <a
